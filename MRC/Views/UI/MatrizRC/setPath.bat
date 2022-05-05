3e3c493cf3ac6d6e4a041c803216c7da4b3c350e
Demian Cruz <30833341+pedrodemiangodinezcruz@users.noreply.github.com>
2022-05-04 20:41:30 -0500

    Cls

--- MRC/Views/UI/MatrizRC/setPath.bat
+++ MRC/Views/UI/MatrizRC/setPath.bat
@@ -1,3 +1,3 @@
-@cd C:\Users\pedro.godinez\Documents\GitHub\Bocar_Matriz
-@set PATH=C:\Users\pedro.godinez\Documents\GitHub\Bocar_Matriz\node-v16.14.0-win-x64\node-v16.14.0-win-x64;%PATH%
+@cd C:\Users\pedro.godinez\Documents\GitHub\MRC\MRC\Views\UI\MatrizRC
+@set PATH=C:\Users\pedro.godinez\Documents\GitHub\MRC\MRC\Views\UI\MatrizRC\node-v16.14.0-win-x64\node-v16.14.0-win-x64;%PATH%
 @cmd.exe /K
