66502d2a68c7e210db97e08c7bdec5408f1b1b33
Demian Cruz <30833341+pedrodemiangodinezcruz@users.noreply.github.com>
2022-05-09 12:09:00 -0500

    Select completo

--- MRC/Views/UI/MatrizRC/src/app/body/matriz/matriz.component.ts
+++ MRC/Views/UI/MatrizRC/src/app/body/matriz/matriz.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {SharedService} from 'src/app/shared.service';
 
 @Component({
   selector: 'app-matriz',
@@ -10,12 +10,30 @@ export class MatrizComponent {
 
 	myScriptElement: HTMLScriptElement;
 	
-    constructor() { 
+    constructor(private service:SharedService) { 
+	
 	this.myScriptElement = document.createElement('script');
 	this.myScriptElement.src = '../../assets/js/matriz.js';
 	document.body.appendChild(this.myScriptElement);
-
-  
+	}
+	RiesgoList:any=[];
+	ControlList:any=[];
+	ngOnInit():void {
+		
+		this.refreshRiesgoList();
+		this.refreshControlList();
+	}
+	refreshRiesgoList() {
+		this.service.getRiesgoList().subscribe(data=> {
+			this.RiesgoList = data;
+			console.log(this.RiesgoList);
+		});
+	}
+	refreshControlList(){
+		this.service.getControlesList().subscribe(datos=> {
+			this.ControlList = datos;
+			console.log(this.ControlList);
+		});
 	}
 
 }
